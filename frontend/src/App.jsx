5bb98a1a43b31142c5f3a457dc603f67d1603dc9
Chris <is>
2025-09-22 20:46:22 -0400

    updated read me

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Top from './components/Top/Top.jsx'
 import Feed from './components/Feed/Feed.jsx'
 
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+import SignUpPage from './pages/SignUpPage';
+import LogInPage from './pages/LogInPage.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <Top></Top>
-        <Feed></Feed>
+
+        <Router>
+          <nav>
+            <Link to="/">Page 1</Link>
+            <Link to="/page2">Page 2</Link>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<SignUpPage></SignUpPage>}></Route>
+            <Route path="/page2" element={<LogInPage></LogInPage>}></Route>
+          </Routes>
+        </Router>
+
+
       </div>
     </>
   )
